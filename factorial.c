23afc9a7f6095d8a5dd063aa31ce2a1b97a68f50
Xise <127363089+Xise@users.noreply.github.com>
2023-05-13 00:25:08 +0330

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fact(int numb)
+{
+
+     int n = numb;
+     int i = 1;
+     int x = 1;
+     while (i < n) // ta vaghti i kamtar az n hast
+     {
+          ++i;
+          x = x * i;
+          printf("i is %d\n", i);
+     }
+     return x;
+}
+
+int main()
+{
+     int result = fact(4);
+     printf("%d\n", result);
+     return 0;
+}
